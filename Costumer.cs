788501b1473b7c8010d8c1a1fb329b2db07468a5
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-02-06 12:26:01 +0300

    Update Costumer.cs

--- Costumer.cs
+++ Costumer.cs
@@ -12,7 +12,7 @@ namespace ClassMethodDemo
     public int Id { get; set; }
     public string Name { get; set; }
     public string Surname { get; set;}
-     
+    public string City { get; set; }
   }
 }
 
