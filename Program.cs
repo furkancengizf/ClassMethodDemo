d23689af90951218e7609ba63d86592e02b96a8a
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-02-06 12:22:37 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using Systems;
+namespace ClassMethodDemo 
+{
+  class Program 
+  {
+     static void main(string[] args)
+     }
+     Costumer.Costumer1 = new Costumer;
+     Costumer1.Name = "Buğra"
+     Costumer1.Surname = "Çelik"
+     Costumer1.Id = 
+  
