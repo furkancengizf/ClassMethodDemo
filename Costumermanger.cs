607970422b7d1349e5c08f9e9adb50124bebe507
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-02-06 12:08:18 +0300

    Update Costumermanger.cs

--- Costumermanger.cs
+++ Costumermanger.cs
@@ -5,4 +5,37 @@ using System.Text;
 namespace ClassMethodDemo
 {
   
- 
+ class CostumerManager 
+ {
+   public void Add(Costumer costumer)
+   {
+     Console.WriteLine("Müşteri Eklendi:"+ costumer.Name);
+     }
+   public void Del(Costumer costumer)
+   {
+     Console.WriteLine(costumer Id""costumer.Name"+"Müşteri Silindi");
+     }
+   public void List(Costumer[] costumers)
+   {
+     foreach (Costumer costumer in costumers)
+     {
+       Console.WriteLine();
+
+                Console.WriteLine("---------------------------");
+
+                Console.WriteLine("Customer ID : " + customer.Id);
+
+                Console.WriteLine("Müşteri İsmi : " + customer.Name);
+
+                Console.WriteLine("Müşteri Soyadı : " + customer.SurName);
+
+                Console.WriteLine("Müşteri Şehiri : " + customer.City);
+
+                Console.WriteLine("Müşteri Telefonu : " + customer.Phone);
+
+                Console.WriteLine("------------------------------");
+
+            }
+       }
+   }
+                       
